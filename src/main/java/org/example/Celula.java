456873297c4ad517810a225c21bff93ade49e73a
LenioMMM <151686695+LenioMMM@users.noreply.github.com>
2023-11-21 19:22:47 -0300

    Add files via upload
    feito código da célula, bem simples com os retorna e define prontos, se preciso posso aumentar o código adicionando as funções necessárias

---
+++ src/main/java/org/example/Celula.java
@@ -0,0 +1,26 @@
+package com.mycompany.hashcelula;
+
+public class Celula {
+    private int id;
+    private Produto produto;
+
+    
+    public int retornaId() {
+        return id;
+    }
+
+    public void defineId(int id) {
+        this.id = id;
+    }
+
+    public Produto retornaProduto() {
+        return produto;
+    }
+
+    public void defineProduto(Produto produto) {
+        this.produto = produto;
+    }
+    
+    
+    
+}
