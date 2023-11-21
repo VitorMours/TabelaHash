64cbda1fff776ae1d38c889ac7f13a68245df1a1
Ricardo-Dias-Xavier <151686655+Ricardo-Dias-Xavier@users.noreply.github.com>
2023-11-21 20:38:32 -0300

    Update Main.java
    adicionado por hora parte do método de comprar produto, possibilitando q ele adicione para o mesmo, a identificação de qual usuário vai fazer a compra

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static ArrayList<Cliente> listaClientes;
-
+    private static int identificacao = 0;
     public static boolean condition = true;
     public static void main(String[] args) {
         int acao = 0;
@@ -12,9 +12,10 @@ public class Main {
 
         do{
             System.out.println("=== Black Friday da HashTable! ===\n");
-            System.out.println("1 - Registrar Cliente:" +
-                               "\n2 - Fazer compra" +
-                               "\n3 - Usar Cupom");
+            System.out.println("""
+                               1 - Registrar Cliente:
+                               2 - Fazer compra: 
+                               3 - Usar Cupom: """);
             System.out.println("==================================");
             System.out.print("Escolha oque você deseja fazer: ");
 
@@ -49,13 +50,35 @@ public class Main {
         }else{
             novoCliente = new Cliente(nome);
         }
-        System.out.println("Cliente registrado:" +"\nNome: "+novoCliente.getNome()+"\nSaldo: "+novoCliente.getSaldo());
-
-
+        System.out.println("""
+                           Cliente registrado:
+                           Nome: """+novoCliente.getNome()+"\nSaldo: "+novoCliente.getSaldo());
+        novoCliente.setIdentificacao(identificacao);
+        identificacao++;
+        listaClientes.add(novoCliente);
     }
+    
     public static void comprarProduto(){
-        System.out.println("Produto comprado");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o ID do usuário");
+        int id = input.nextInt();
+        for(Cliente cliente: listaClientes){
+            if (cliente.getIdentificacao() == id){
+                System.out.println("O cliente que deseja fazer a compra é:\n"
+                        +"nome"+cliente.getNome()
+                        );
+            }
+        
+                
+        }
+
+
+        
     }
+    
+    
+    
+    
     public static void gastarCupom(){
         System.out.println("Cupom gasto");
 
